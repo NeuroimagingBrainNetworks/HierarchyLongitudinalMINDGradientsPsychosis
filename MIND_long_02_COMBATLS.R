6494e046ddc0b13b53798d44a4de075910a3036d
Natalia García-San-Martín <132894441+nasamar@users.noreply.github.com>
2026-01-26 15:17:21 +0100

    Update MIND_long_02_COMBATLS.R

--- MIND_long_02_COMBATLS.R
+++ MIND_long_02_COMBATLS.R
@@ -7,18 +7,18 @@
 #  Written by Natalia García San Martín (ngarcia1@us.es)
 # 
 # 
-#  Hierarchy MIND Psychosis toolkit is free software: 
+#  Hierarchy Longitudinal Gradients Psychosis toolkit is free software: 
 #  you can redistribute it and/or modify it under the terms of the 
 #  GNU General Public License as published by the Free Software Foundation, 
 #  either version 3 of the License, or (at your option) any later version.
 # 
-#  Hierarchy MIND Psychosis toolkit is distributed in the hope that 
+#  Hierarchy Longitudinal Gradients Psychosis toolkit is distributed in the hope that 
 #  it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
 #  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 # 
 #  You should have received a copy of the GNU General Public License
-#  along with Hierarchy MIND Psychosis toolkit. If not, see 
+#  along with Hierarchy Longitudinal Gradients Psychosis toolkit. If not, see 
 #  <https://www.gnu.org/licenses/>.
 
 rm(list=ls()) # Previous data cleaning in memory
@@ -227,6 +227,7 @@ for (type in types){
   }
   
 }
+
 
 
 
